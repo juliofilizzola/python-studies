76116e912ffa215d8dd41a38b0343611b25d0174
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-01-06 18:39:49 -0300

    convert  python

---
+++ 1/init-python/convert.py
@@ -0,0 +1,28 @@
+# Conversão de int para float
+inteiro = 10
+ponto_flutuante = float(inteiro)
+print(f"Inteiro: {inteiro}, Ponto Flutuante: {ponto_flutuante}")
+
+print('===============================================')
+
+# conversão de float para int
+
+ponto_flutuante = 5.9
+inteiro = int(ponto_flutuante)
+print(f"Ponto Flutuante: {ponto_flutuante}, Inteiro: {inteiro}")
+
+print('===============================================')
+
+# conversão de int para str
+
+inteiro = 42
+texto = str(inteiro)
+print(f"Inteiro: {inteiro}, Texto: {texto}")
+
+print('===============================================')
+
+# conversão de str em float
+
+texto = "3.14"
+ponto_flutuante = float(texto)
+print(f"Texto: {texto}, Ponto Flutuante: {ponto_flutuante}")
