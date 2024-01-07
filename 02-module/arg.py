fdf8a40cde63755dc4c86821f17f6184fd823ff1
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-01-06 21:34:49 -0300

    value nomed

--- 02-module/arg.py
+++ 02-module/arg.py
@@ -10,3 +10,14 @@ def some_func(x):
 
 
 print(some_func(x=334))
+
+
+def some_func2(x, y=None):
+	if y is None:
+		return x * 3
+	else:
+		return x + y * 8
+	
+
+print(some_func2(x=3))
+print(some_func2(x=6, y=4))
